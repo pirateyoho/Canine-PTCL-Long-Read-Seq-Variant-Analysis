ab5dfc8378b299cafed849cdae5fb2dfebc87b71
Eileen O. DVM DACVP <134553786+pirateyoho@users.noreply.github.com>
2025-01-05 10:55:25 -0700

    Add files via upload

---
+++ 03-pbsv-discover.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=pbsv_discover
+#SBATCH --nodes=1
+#SBATCH --ntasks=24
+#SBATCH --time=23:00:00
+#SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
+#SBATCH --mail-user=edlarsen@colostate.edu
+#SBATCH --output=pbsv_discover_log_%j.txt
+
+
+# Print pbsv version
+pbsv --version
+
+for file in $(ls ../output/*.aligned.bam)
+do
+name=$(basename ${file} .aligned.bam); echo ${name}
+pbsv discover ${file} ${name}.svsig.gz
+done
