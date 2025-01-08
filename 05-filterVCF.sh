bb5aa33549b66398460627fb581c00ed9fd98631
Eileen O. DVM DACVP <134553786+pirateyoho@users.noreply.github.com>
2025-01-08 11:57:00 -0700

    Update 05-filterVCF.sh

--- 05-filterVCF.sh
+++ 05-filterVCF.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
+#SBATCH --job-name=filterVCF
+#SBATCH --nodes=1
+#SBATCH --ntasks=16
+#SBATCH --time=1:00:00
+#SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
+#SBATCH --mail-user=edlarsen@colostate.edu
+#SBATCH --output=filterVCF_log_%j.txt
 
-# Remove variants that failed pbsv filters
-bcftools view -i 'FILTER="PASS"' ../output/PTCL_StructuralVariants_CanFam3.vcf.vcf > ../output/PTCL_StructuralVariants_CanFam3.vcf.pass.vcf
+######## install svpack ########
+git clone https://github.com/PacificBiosciences/svpack.git
+cd svpack
 
-# Create vcf file of only breakend variant calls
-bcftools view -i 'INFO/SVTYPE=="BND"' ../output/PTCL_StructuralVariants_CanFam3.vcf.pass.vcf > ../output/PTCL_BND_Variants.pass.vcf
+######## Return PASS SVs at least 50 bp long ########
+./svpack filter --pass-only --min-svlen 50 ../../output/PTCL_StructuralVariants_CanFam3.vcf > ../../output/PTCL_StructuralVariants_CanFam3.filtered.vcf
+
+######## Annotate SVs that impact genes ########
+# Retrieve gff3 file for reference genome
+rsync -azvP rsync://ftp.ensembl.org/ensembl/pub/release-104/gff3/canis_lupus_familiaris/Canis_lupus_familiaris.CanFam3.1.104.gff3.gz .
+rsync -azvP rsync://ftp.ensembl.org/ensembl/pub/release-104/gff3/canis_lupus_familiaris/CHECKSUMS .
+grep "CanFam3.1.104.gff3.gz" CHECKSUMS
+sum Canis_lupus_familiaris.CanFam3.1.104.gff3.gz
+gunzip Canis_lupus_familiaris.CanFam3.1.104.gff3.gz
+
+# Add BCSQ tag to variants that impact genes
+./svpack consequence ../../output/PTCL_StructuralVariants_CanFam3.filtered.vcf Canis_lupus_familiaris.CanFam3.1.104.gff3 > ../../output/PTCL_StructuralVariants_CanFam3.filtered.ann.vcf
+
+######## Subset only SVs that impact genes ########
+bcftools view -i 'INFO/BCSQ!=""' ../../output/PTCL_StructuralVariants_CanFam3.filtered.ann.vcf -o ../../output/PTCL_StructuralVariants_CanFam3.filtered.ann.bcsq.vcf
+
+######## Create vcf file of only breakend variant calls ########
+# Of all variants that passed filtering
+bcftools view -i 'INFO/SVTYPE=="BND"' ../../output/PTCL_StructuralVariants_CanFam3.filtered.ann.vcf -o ../../output/PTCL_BND_Variants.filtered.ann.vcf
+# Of only variants that impact genes
+bcftools view -i 'INFO/SVTYPE=="BND"' ../../output/PTCL_StructuralVariants_CanFam3.filtered.ann.bcsq.vcf -o ../../output/PTCL_BND_Variants.filtered.ann.bcsq.vcf
